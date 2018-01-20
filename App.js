94e19568d66863fb3f83e6741b098b812fdd6995
Abhishek Alai <abbhishek971@gmail.com>
2018-01-20 11:08:31 +0530

    Second Commit: Almost finished with the app

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 
 import Store from './src/Store';
 import { Header } from './src/components';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const { container } = styles;
 
 export default class App extends React.Component {
   render() {
@@ -16,12 +27,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
